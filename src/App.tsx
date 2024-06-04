773a0753867b3dd780998b94084b85f946aae401
Mani-Sankar01 <sm7532567@gmail.com>
2024-06-04 19:52:12 +0530

    Initiate project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-slate-400 p-2 rounded-md">
+        Hello world!
+      </h1>
+      <button>Hello</button>
+    </>
+  );
+}
+
+export default App;
